a32ff9bdf6bc7ea3b099e80d92318b25f2024266
Henk Hornschuh <h.hornschuh@basecom.de>
2024-11-14 08:55:55 +0100

    Initial Commit

---
+++ Vision-BBS/AppModel.swift
@@ -0,0 +1,21 @@
+//
+//  AppModel.swift
+//  Vision-BBS
+//
+//  Created by Henk Hornschuh on 14.11.24.
+//
+
+import SwiftUI
+
+/// Maintains app-wide state
+@MainActor
+@Observable
+class AppModel {
+    let immersiveSpaceID = "ImmersiveSpace"
+    enum ImmersiveSpaceState {
+        case closed
+        case inTransition
+        case open
+    }
+    var immersiveSpaceState = ImmersiveSpaceState.closed
+}
---
+++ Vision-BBS/Vision_BBSApp.swift
@@ -0,0 +1,33 @@
+//
+//  Vision_BBSApp.swift
+//  Vision-BBS
+//
+//  Created by Henk Hornschuh on 14.11.24.
+//
+
+import SwiftUI
+
+@main
+struct Vision_BBSApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+     }
+}
